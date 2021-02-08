39e87ae8fb39ba111f5b4d59ca10e5bd65ed436c
Sayan <55659614+TechFriends1234@users.noreply.github.com>
2021-02-08 18:13:36 +0530

    Initial sharing of project

---
+++ BubbleSort_SAYAN.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class BubbleSort_SAYAN here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BubbleSort_SAYAN
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BubbleSort_SAYAN
+     */
+    public BubbleSort_SAYAN()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
